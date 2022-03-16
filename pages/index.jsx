120e800b540d58b25470ad9cb1fcdd1a192445a0
github naoki4287 <github@waka.naoki86@icloud.com>
2022-03-16 09:36:33 +0900

    🚚 jsからjsxに名前を変更しました

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <p className="text-4xl">Hello Next.js</p>
+
+      
+    </Layout>
+  );
+}
