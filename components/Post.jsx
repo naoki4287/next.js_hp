af76315e1e8b46fe091da118181eb39aa83a2e51
github naoki4287 <github@waka.naoki86@icloud.com>
2022-03-16 10:42:08 +0900

    getStaticPathsでDynamic routingを実装しました

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
+
 export const Post = ({ post }) => {
-  return <div>
-    <span>{post.id}</span>
-    {" : "}
-    <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">{post.title}</span>
-  </div>;
+  return (
+    <div>
+      <span>{post.id}</span>
+      {" : "}
+      <Link href={`/posts/${post.id}`}>
+        <a><span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
+          {post.title}
+        </span></a>
+      </Link>
+    </div>
+  );
 };
 
 export default Post;
